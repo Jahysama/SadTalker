414f64f3fb64dd61fa0f808ed8acb846a26a37fb
Egor <megametagross@outlook.de>
2023-04-25 14:28:35 +0300

    fixed video saving

--- src/utils/videoio.py
+++ src/utils/videoio.py
@@ -18,14 +18,5 @@ def load_video_to_cv2(input_path):
     return full_frames
 
 def save_video_with_watermark(video, audio, save_path, watermark=None):
-    temp_file = str(uuid.uuid4())+'.mp4'
-    cmd = r'ffmpeg -filter_complex "scale=w=480:h=-1:flags=lanczos -y -i "%s" -i "%s" -vcodec copy "%s"' % (video, audio, temp_file)
+    cmd = r'ffmpeg -filter_complex "scale=w=480:h=-1:flags=lanczos -y -i "%s" -i "%s" -vcodec copy "%s"' % (video, audio, save_path)
     os.system(cmd)
-
-    if not watermark:
-        shutil.move(temp_file, save_path)
-    else:
-        # watermark
-        cmd = r'ffmpeg -filter_complex "scale=w=480:h=-1:flags=lanczos -y -hide_banner -i "%s" -i "%s" -filter_complex "[1]scale=100:-1[wm];[0][wm]overlay=(main_w-overlay_w)-10:10" "%s"' % (temp_file, watermark, save_path)
-        os.system(cmd)
-        os.remove(temp_file)
