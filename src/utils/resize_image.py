ac9bc194c66bcd80153dbc0ef27df9b4875ab9e8
Egor <megametagross@outlook.de>
2023-04-26 14:36:13 +0300

    fixed image resize

--- src/utils/resize_image.py
+++ src/utils/resize_image.py
@@ -1,20 +1,27 @@
 import numpy as np
-from PIL import Image
+import cv2
+from os.path import splitext
 from skimage import io, transform
 
 
 def resize_image(pic_path: str):
-    img = Image.open(pic_path)
-    width, height = img.size
+    img = cv2.imread(pic_path)
+    width, height, channels = img.shape
+    if channels < 4:
+        print('converting to png')
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGBA)
     if width > 512:
         percent = 512/width
-        width = 512
-        height = int(height*percent)
+        width_new = 512
+        height_new = int(height*percent)
     elif height > 768:
         percent = 768/height
-        height = 768
-        width = int(width*percent)
+        height_new = 768
+        width_new = int(width*percent)
+    else:
+        height_new = height
+        width_new = width
 
-    source_image = np.array(img)
-    source_image = transform.resize(source_image, (height, width, 3))
-    io.imsave(pic_path, source_image)
+    source_image = img.astype(np.uint8)
+    source_image = transform.resize(source_image, (width_new, height_new))
+    io.imsave(f'{splitext(pic_path)[0]}.png', source_image)
